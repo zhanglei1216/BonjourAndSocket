5152760ddb5b41b00c5ca4ebfd86e0bbc514973f
zhanglei <zhanglei1216@163.com>
2015-04-13 17:51:11 +0800

    add file

---
+++ BonjourSocket/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BonjourSocket
+//
+//  Created by foreveross－bj on 15/4/13.
+//  Copyright (c) 2015年 foreveross－bj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
